d248099ebbd380ac5ca01d31706091df7c7371f1
Joseph Santoyo <josephsantoyo@WINDOWS.SCS.CARLETON.CA>
2017-07-17 18:41:22 -0400

    implement loop that counts the number of heads/tails based on a given probability

--- tutorial-02/Flip.java
+++ tutorial-02/Flip.java
@@ -3,21 +3,34 @@
 import java.util.Random;
 
 public class Flip{
-	
-	public static void main(String[] args){
-		double bias = 0.5;  // 50% heads, 50% tails
-		boolean ok = true;
-		if(args.length == 1){
-			try{
-				bias = Double.parseDouble(args[0]);
-			}catch(Exception e){
-				System.out.println("command line argument must be a number");
-				ok = false;
-			}
-		}
-		if(ok){
-			System.out.print("bias is " + bias);
-		}
-	}
+ 
+ public static void main(String[] args){
+  double bias = 0.5;  // 50% heads, 50% tails
+  boolean ok = true;
+  if(args.length == 1){
+   try{
+    bias = Double.parseDouble(args[0]);
+   }catch(Exception e){
+    System.out.println("command line argument must be a number");
+    ok = false;
+   }
+  }
+  if(ok){ 
+   System.out.print("bias is " + bias);
+   
+   Random r = new Random();
+   
+   int num_heads = 0;
+   int num_tails = 0;
+   for (int i = 0; i < 100; i++) {
+     if (r.nextDouble() < bias) { 
+       num_heads++;
+     } else {
+       num_tails++;
+     }
+   }
+   System.out.print(", " + num_heads + " heads, " + num_tails + " tails.\n");
+  }
+ }
 }
 
